f35d8a6f96eff6953d0d3122896610b25344cafd
t0nnaam <tn.sima.394@gmail.com>
2026-02-08 03:51:30 -0800

    actor_update, update ppo innit, rewrote network

--- ppo/network.py
+++ ppo/network.py
@@ -1,59 +1,85 @@
 
-import tensorflow as tf
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Dense, Concatenate
-from config import ACTION_SPACE_SIZE
+import torch
+import torch.nn as nn
 import numpy as np
+
+ACTION_SPACE_SIZE = 12
 
 #Actor state input consists of IMU (6), Servo (12) if Lidar is added (3)
 
-# COPIED FROM REFERENCE CODE - Most should be usable but some stuff should be changed for PPO
-class Actor:
-    def actor_model(self):
-        imu_input = Input(shape =(6,), name="IMU_input") 
-        x_imu = Dense(64, activation='relu')(imu_input)
-        x_imu = Dense(32, activation='relu')(x_imu)
-       
-        servo_input = Input(shape=(12,), name="Servo_input")
-        x_servo = Dense(64, activation='relu')(servo_input)
-        x_servo = Dense(32, activation='relu')(x_servo)
-
-        lidar_input = Input(shape=(3,), name="Lidar_input")
-        x_lidar = Dense(64, activation='relu')(lidar_input)
-        x_lidar = Dense(32, activation='relu')(x_lidar)
-
-        combined_inputs = Concatenate()([x_imu, x_servo, x_lidar])
-        x = Dense(64, activation='relu')(combined_inputs)
+# I changed the NN to use pytorch instead of tensorflow cuz that's what was covered last session
+class Actor(nn.Module): 
+    def __init__(self):
+        super(Actor, self).__init__()
         
-        # Output should be changed
-        output = Dense(ACTION_SPACE_SIZE, activation='tanh')(x)
+        self.imu_fc1 = nn.Linear(6, 64)
+        self.imu_fc2 = nn.Linear(64, 32)
         
-        actor_model = Model(inputs=[imu_input, servo_input, lidar_input], outputs=output)
+        self.servo_fc1 = nn.Linear(12, 64)
+        self.servo_fc2 = nn.Linear(64, 32)
+        
+        self.lidar_fc1 = nn.Linear(3, 64)
+        self.lidar_fc2 = nn.Linear(64, 32)
+        
+        self.combined_input = nn.Linear(96, 64)  # 32+32+32 -> 64
+        
+        self.mean_layer = nn.Linear(64, ACTION_SPACE_SIZE)
+        self.log_std_layer = nn.Linear(64, ACTION_SPACE_SIZE)
+        
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
 
-        return actor_model
+        def forward(self, imu, servo, lidar):
+            x_imu = self.relu(self.imu_fc1(imu))
+            x_imu = self.relu(self.imu_fc2(x_imu))
+            
+            x_servo = self.relu(self.servo_fc1(servo))
+            x_servo = self.relu(self.servo_fc2(x_servo))
 
-class Critic:
-    #state and action inputs
-    def critic_model(self):
-        imu_input = Input(shape =(6,), name="IMU_input") 
-        x_imu = Dense(64, activation='relu')(imu_input)
-        x_imu = Dense(32, activation='relu')(x_imu)
-       
-        servo_input = Input(shape=(12,), name="Servo_input")
-        x_servo = Dense(64, activation='relu')(servo_input)
-        x_servo = Dense(32, activation='relu')(x_servo)
+            x_lidar = self.relu(self.lidar_fc1(lidar))
+            x_lidar = self.relu(self.lidar_fc2(x_lidar))
+            
+            combined = torch.cat([x_imu, x_servo, x_lidar], dim=-1)
+            x = self.relu(self.combined_fc(combined))
+            
+            mean = self.tanh(self.mean_layer(x))
+            log_std = self.tanh(self.log_std_layer(x))
+            
+            return mean, log_std
 
-        lidar_input = Input(shape=(3,), name="Lidar_input")
-        x_lidar = Dense(64, activation='relu')(lidar_input)
-        x_lidar = Dense(32, activation='relu')(x_lidar)
+class Critic(nn.Module):
+    def __init__(self):
+        super(Critic, self).__init__()
+        
+        self.imu_fc1 = nn.Linear(6, 64)
+        self.imu_fc2 = nn.Linear(64, 32)
+        
+        self.servo_fc1 = nn.Linear(12, 64)
+        self.servo_fc2 = nn.Linear(64, 32)
 
-        # action_input = Input(shape=(ACTION_SPACE_SIZE,), name="Action_input")
-        combined_inputs = Concatenate()([x_imu, x_servo, x_lidar])
-        x = Dense(64, activation='relu')(combined_inputs)
-
-        #q value output
-        output = Dense(1, activation='linear')(x)
-
-        critic_model = Model(inputs=[imu_input, servo_input, lidar_input], outputs=output)
-
-        return critic_model
+        self.lidar_fc1 = nn.Linear(3, 64)
+        self.lidar_fc2 = nn.Linear(64, 32)
+        
+        self.combined_fc = nn.Linear(96, 64)  # 32+32+32 -> 64
+        
+        self.value_layer = nn.Linear(64, 1)
+        
+        self.relu = nn.ReLU()
+    
+    def forward(self, imu, servo, lidar):
+        x_imu = self.relu(self.imu_fc1(imu))
+        x_imu = self.relu(self.imu_fc2(x_imu))
+        
+        x_servo = self.relu(self.servo_fc1(servo))
+        x_servo = self.relu(self.servo_fc2(x_servo))
+        
+        x_lidar = self.relu(self.lidar_fc1(lidar))
+        x_lidar = self.relu(self.lidar_fc2(x_lidar))
+        
+        combined = torch.cat([x_imu, x_servo, x_lidar], dim=-1)
+        x = self.relu(self.combined_fc(combined))
+        
+        value = self.value_layer(x)
+        
+        return value
+    
--- ppo/ppo.py
+++ ppo/ppo.py
@@ -2,35 +2,41 @@
 from network import Actor, Critic
 # from replay_buffer import ReplayBuffer
 import random
-import tensorflow as tf
+import torch
 import numpy as np
-from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras.optimizers import Adam
 
 # Make a branch when you work on the code and then push it to the repo
-
+ 
 # I started an outline but we still need to change the parameters on the functions
 
 class PPO:
-    def __init__(self, discount, clipping, advantage, batch_size):
+    def __init__(self, discount=0.99, clipping=0.2, advantage=0.9, epoch=10, batch_size=64):
+        # the values here are sorta placeholders rn - we can test and change later
         # Networks
         self.actor = Actor()
         self.critic = Critic()
         
         # Optimizers
-        self.actor_optimizer = Adam(learning_rate=0.001)
-        self.critic_optimizer = Adam(learning_rate=0.001)
+        self.actor_optimizer = torch.optim.Adam(learning_rate=0.001)
+        self.critic_optimizer = torch.optim.Adam(learning_rate=0.001)
         
         # Hyperparameters
         self.discount = discount  # discount factor
         self.clipping = clipping  # PPO clipping parameter
         self.advantage = advantage # for advantage estimation
-        self.batch_size = batch_size
+        self.epochs = epoch # number of epochs
+        self.batch_size = batch_size # number of samples per update
         
         # Storage for trajectories
-        self.states = []
+        self.states = {'imu': [], 'servo': [], 'lidar':[]}
         self.actions = []
         self.rewards = []
         self.values = []
+        self.log_probs = [] # store old log probs for PPO
+
+    def compute_log_prob(self, imu, servo, lidar, actions):
+        pass
 
     def rollout(self):
         """ Get trajectory data by executing policy within environment """
@@ -48,15 +54,32 @@ class PPO:
         """ Main update function - call actor and critic updates"""
         pass
     
-    def update_actor(self):
+    def updateActor(self, imu, servo, lidar, actions, log_prob_old):
         """ Update policy using clipped PPO objective"""
-        pass
+        # with torch.GradientTape() as tape:
+        log_prob_new = self.compute_log_prob(imu, servo, lidar, actions)
+        
+        # Calculate probability ratio = pi_theta(a_t | s_t) / pi_theta_k(a_t | s_t)
+        ratio = torch.exp(log_prob_new - log_prob_old)
     
-    def update_critic(self, states, returns):
+        # Calculate clipped vs unclipped loss
+        unclippedL = ratio * self.advantage 
+        ratio_clipped  = torch.clamp(ratio, 1 - self.clipping, 1 + self.clipping)
+        clippedL = ratio_clipped  * self.advantage
+
+        # actor loss = pessimistic estimate, take minimum
+        actor_loss = (-torch.min(unclippedL, clippedL)).mean()
+
+        # Calculate gradients, backward propagation for actor network
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+    
+    def updateCritic(self, states, returns):
         """ Update value function using MSE loss function"""
         pass
     
-    def clear_memory(self):
+    def clearMemory(self):
         """ Clear trajectory buffers after update"""
         pass
 
